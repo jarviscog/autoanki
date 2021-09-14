ef35d072bbbe46bb4a3d2a357c6c4f5a626282b7
BerthaCoghlin <jarviscoghlin@gmail.com>
2021-09-13 22:09:10 -0400

    Reformatted web_assets.py

--- web_assests.py
+++ web_assests.py
@@ -8,25 +8,25 @@ from selenium.webdriver.chrome.options import Options
 import time
 import os
 
-def getPinyin(filename):
 
+def get_pinyin(filename, headless=True):
     file_to_be_created = os.path.splitext(filename)[0] + "_pinyin.txt"
 
     # FIXME Remove hard-coding of downloaded filepath
-    downloadedFilepath = "C:\\Users\\jarvi\\Downloads\\" + filename
+    downloaded_filepath = "C:\\Users\\jarvi\\Downloads\\" + filename
 
-    if(os.path.exists(file_to_be_created)):
+    if os.path.exists(file_to_be_created):
         print('getPinyin() already done')
         return file_to_be_created
 
-    if os.path.exists(downloadedFilepath):
-    #     File has already been downloaded
+    if os.path.exists(downloaded_filepath):
+        # File has already been downloaded
         print("File already downloaded")
     else:
         options = Options()
         # prefs = {"download.default_directory": "/some/path"}
         # options.add_experimental_option("prefs", prefs)
-        options.headless = False
+        options.headless = headless
         url = 'https://www.purpleculture.net/chinese-pinyin-converter/'
         driver = webdriver.Chrome(options=options, executable_path=str(os.getcwd() + "\\" + 'chromedriver.exe'))
         driver.get(url)
@@ -37,25 +37,24 @@ def getPinyin(filename):
         uploadBox.send_keys(os.getcwd() + "\\" + filename)
         convertButton = driver.find_element_by_xpath('//*[@id="fileuploadform"]/div[2]/div/button[1]')
         convertButton.click()
-        #Wait until file is in downloads file. Times out after 15 seconds
+        # Wait until file is in downloads file. Times out after 15 seconds
         for i in range(30):
-            if(os.path.exists(downloadedFilepath)):
+            if (os.path.exists(downloaded_filepath)):
                 break
             time.sleep(0.5)
-            if(i == 29):
+            if (i == 29):
                 print("getPinyin File not downloaded correctly")
         driver.quit()
 
     # The file with the characters and pinyin
-    pinyinFile = open(downloadedFilepath, "r", encoding='utf-8')
+    pinyinFile = open(downloaded_filepath, "r", encoding='utf-8')
     # The new file to be created
     newFileName = os.path.splitext(filename)[0] + "_pinyin.txt"
 
     newFile = open(newFileName, "w", encoding='utf-8')
-    length = int(general_functions.file_len(downloadedFilepath)/2)
+    length = int(general_functions.file_len(downloaded_filepath) / 2)
 
     for i in range(length):
-
         pinyin = pinyinFile.readline().strip()
         originalFile = pinyinFile.readline().replace("\n", "").strip()
 
@@ -65,8 +64,8 @@ def getPinyin(filename):
 
     return newFileName
 
-def getPinyinNumbers(filename):
 
+def get_pinyin_numbers(filename, headless=True):
     file_to_be_created = os.path.splitext(filename)[0] + "_pinyinNum.txt"
 
     print(file_to_be_created)
@@ -74,12 +73,12 @@ def getPinyinNumbers(filename):
     # FIXME Remove hard-coding of downloaded filepath
     downloadedFilepath = "C:\\Users\\jarvi\\Downloads\\" + file_to_be_created
 
-    if(os.path.exists(file_to_be_created)):
+    if (os.path.exists(file_to_be_created)):
         print('getPinyinNumbers() already done')
         return file_to_be_created
 
     if os.path.exists(downloadedFilepath):
-    #     File has already been downloaded
+        #     File has already been downloaded
         print("File already downloaded")
     else:
 
@@ -88,7 +87,7 @@ def getPinyinNumbers(filename):
         downloadedFilepath = "C:\\Users\\jarvi\\Downloads\\" + cut_filename
 
         options = Options()
-        options.headless = False
+        options.headless = headless
         url = 'https://www.purpleculture.net/chinese-pinyin-converter/'
         driver = webdriver.Chrome(options=options, executable_path=str(os.getcwd() + "\\" + 'chromedriver.exe'))
         driver.get(url)
@@ -103,12 +102,12 @@ def getPinyinNumbers(filename):
 
         convertButton = driver.find_element_by_xpath('//*[@id="fileuploadform"]/div[2]/div/button[1]')
         convertButton.click()
-        #Wait until file is in downloads file. Times out after 15 seconds
+        # Wait until file is in downloads file. Times out after 15 seconds
         for i in range(30):
-            if(os.path.exists(downloadedFilepath)):
+            if (os.path.exists(downloadedFilepath)):
                 break
             time.sleep(0.5)
-            if(i == 29):
+            if (i == 29):
                 print("getPinyinNum File not downloaded correctly")
                 return
         driver.quit()
@@ -121,10 +120,9 @@ def getPinyinNumbers(filename):
     wordListFile = open(filename, 'r', encoding='utf-8')
 
     newFile = open(newFileName, "w", encoding='utf-8')
-    length = int(general_functions.file_len(downloadedFilepath)/2)
+    length = int(general_functions.file_len(downloadedFilepath) / 2)
 
     for i in range(length):
-
         pinyin = pinyinFile.readline().strip()
         originalFileCut = pinyinFile.readline().replace("\n", "").strip()
         wordListFileLine = wordListFile.readline().replace("\n", "").strip()
@@ -134,7 +132,8 @@ def getPinyinNumbers(filename):
 
     return newFileName
 
-def getDefinitions(filename, new_filename, number_of_definitions_to_add):
+
+def get_definitions(filename, new_filename, number_of_definitions_to_add, headless=True):
     '''
     Takes a file with 1 definition per line, and downloads definitions for each
         filename: the file to be read from. The output file will be the filename with _definitions appended
@@ -143,21 +142,21 @@ def getDefinitions(filename, new_filename, number_of_definitions_to_add):
     f = open(filename, "r", encoding='utf-8')
 
     options = Options()
-    options.headless = True
+    options.headless = headless
     url = 'https://www.yellowbridge.com/chinese/dictionary.php'
     definitionsDict = {}
 
-    while(number_of_definitions_to_add > 0):
+    while (number_of_definitions_to_add > 0):
 
         line = f.readline()
         lineArr = line.split("&")
         chars = lineArr[0]
         hasDefinition = False
         for i in range(len(lineArr)):
-            if(lineArr[i].split(':')[0].strip() == "definition"):
+            if (lineArr[i].split(':')[0].strip() == "definition"):
                 print("There is a definition here " + chars)
                 hasDefinition = True
-        if(hasDefinition):
+        if (hasDefinition):
             continue
 
         driver = webdriver.Chrome(options=options, executable_path=str(os.getcwd() + "\\" + 'chromedriver.exe'))
@@ -171,7 +170,8 @@ def getDefinitions(filename, new_filename, number_of_definitions_to_add):
 
             mainData = driver.find_element_by_id('mainData')
             definitionData = str(mainData.get_property('innerText'))
-            englishDefinitions = definitionData.split('\n')[1].replace('English Definition','').replace('\t', '').split(';')
+            englishDefinitions = definitionData.split('\n')[1].replace('English Definition', '').replace('\t',
+                                                                                                         '').split(';')
             print("The definition for " + chars + " is ")
 
             definitionsLine = ""
@@ -182,7 +182,7 @@ def getDefinitions(filename, new_filename, number_of_definitions_to_add):
             definitionsDict[chars] = definitionsLine
             print(definitionsLine)
 
-            number_of_definitions_to_add -=1
+            number_of_definitions_to_add -= 1
 
         except Exception as e:
 
@@ -214,14 +214,12 @@ def getDefinitions(filename, new_filename, number_of_definitions_to_add):
         newFile.write(lineToAdd)
 
 
-
-
-
-
-def getAudio(filename):
+def get_audio(filename):
     '''
-    Downloads the pinyin audio clips based off a file with a list of pinyin. Downloading all audio is 2049 files. Some downloaded files will be invalid
-    because some sounds like bang5 do not exist.
+    Downloads the pinyin audio clips based off a file with a list of pinyin. Downloading all audio is 2049 files.
+    Some downloaded files will be invalid because some sounds like bang5 do not exist.
+    This will download a html 404 error page
+    :parameter filename
     '''
     print('getAudio()')
     pinyinToDownload = open(filename, "r", encoding='utf-8')
@@ -232,22 +230,23 @@ def getAudio(filename):
         line = pinyinToDownload.readline().strip('\n').strip()
 
         # Used to check if the file has already been downloaded
-        fileExists = {1:False,2:False,3:False,4:False,5:False}
+        fileExists = {1: False, 2: False, 3: False, 4: False, 5: False}
         requesters = {}
         for i in range(5):
 
-            if (os.path.exists("audio\\" + line + str(i+1) + '.mp3')):
-                print('Found a file in audio: ' + line + str(i+1))
-                fileExists[i+1] = True
+            if (os.path.exists("audio\\" + line + str(i + 1) + '.mp3')):
+                print('Found a file in audio: ' + line + str(i + 1))
+                fileExists[i + 1] = True
 
-            if(fileExists[i+1] == False):
-                requesters[i+1] = requests.get(str('https://r.yellowbridge.com/sounds/py-cbr/' + line + str(i+1) + '.mp3'), allow_redirects=True)
+            if (fileExists[i + 1] == False):
+                requesters[i + 1] = requests.get(
+                    str('https://r.yellowbridge.com/sounds/py-cbr/' + line + str(i + 1) + '.mp3'), allow_redirects=True)
                 time.sleep(1)
 
-                open(str('audio\\' + line + str(i+1) + '.mp3'), 'wb').write(requesters[i+1].content)
+                open(str('audio\\' + line + str(i + 1) + '.mp3'), 'wb').write(requesters[i + 1].content)
     print('getAudio() done')
 
-def getImages(filename):
 
-    #FIXME Finish image grabber
+def getImages(filename):
+    # FIXME Finish image grabber
     return "Get definitions is a WIP"
