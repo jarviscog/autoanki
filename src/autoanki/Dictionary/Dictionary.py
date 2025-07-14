69e93e182775d8669b7f30a0c1e6c796144c14db
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-07-14 11:43:17 -0400

    Added more abstract methods to Dictionary.py

--- src/autoanki/Dictionary/Dictionary.py
+++ src/autoanki/Dictionary/Dictionary.py
@@ -7,6 +7,18 @@ class Dictionary(ABC):
         pass
 
     @abstractmethod
+    def name(self, debug_level=20):
+        pass
+
+    @abstractmethod
+    def description(self, debug_level=20):
+        pass
+
+    @abstractmethod
+    def link(self, debug_level=20):
+        pass
+
+    @abstractmethod
     def find_word(self, word: str) -> None | dict[str, str]:
         pass
 
